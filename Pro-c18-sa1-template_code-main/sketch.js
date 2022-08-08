9f525f099c8bd4833788c4cbbd546ba204e152aa
rhnvenu <109427183+rhnvenu@users.noreply.github.com>
2022-08-08 10:48:37 -0400

    Add files via upload

---
+++ Pro-c18-sa1-template_code-main/sketch.js
@@ -0,0 +1,15 @@
+var s1,s2
+function setup() {
+  createCanvas(400,400);
+s1=new Student("Rohan",13,8)
+
+s2=new Student("Person",30,2)
+s1.display()
+s2.display()
+}
+
+function draw() 
+{
+  background(30);
+  
+}
